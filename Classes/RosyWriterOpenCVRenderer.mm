2584297ae84f53a37deec2f03a3da5f3a4b8a30e
seemakamath <seema.kamath@gmail.com>
2015-02-05 13:54:09 -0800

    Detect rectangles in real-time and color it.

--- Classes/RosyWriterOpenCVRenderer.mm
+++ Classes/RosyWriterOpenCVRenderer.mm
@@ -1,8 +1,8 @@
 
 /*
-     File: RosyWriterOpenCVRenderer.mm
+ File: RosyWriterOpenCVRenderer.mm
  Abstract: n/a
-  Version: 2.1
+ Version: 2.1
  
  Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
  Inc. ("Apple") in consideration of your agreement to the following
@@ -60,53 +60,185 @@
 
 - (BOOL)operatesInPlace
 {
-	return YES;
+    return YES;
 }
 
 - (FourCharCode)inputPixelFormat
 {
-	return kCVPixelFormatType_32BGRA;
+    return kCVPixelFormatType_32BGRA;
 }
 
 - (void)prepareForInputWithFormatDescription:(CMFormatDescriptionRef)inputFormatDescription outputRetainedBufferCountHint:(size_t)outputRetainedBufferCountHint
 {
-	// nothing to do, we are stateless
+    // nothing to do, we are stateless
 }
 
 - (void)reset
 {
-	// nothing to do, we are stateless
+    // nothing to do, we are stateless
 }
 
 - (CVPixelBufferRef)copyRenderedPixelBuffer:(CVPixelBufferRef)pixelBuffer
 {
-	CVPixelBufferLockBaseAddress( pixelBuffer, 0 );
-	
-	unsigned char *base = (unsigned char *)CVPixelBufferGetBaseAddress( pixelBuffer );
-	size_t width = CVPixelBufferGetWidth( pixelBuffer );
-	size_t height = CVPixelBufferGetHeight( pixelBuffer );
-	size_t stride = CVPixelBufferGetBytesPerRow( pixelBuffer );
-	size_t extendedWidth = stride / sizeof( uint32_t ); // each pixel is 4 bytes/32 bits
-	
-	// Since the OpenCV Mat is wrapping the CVPixelBuffer's pixel data, we must do all of our modifications while its base address is locked.
-	// If we want to operate on the buffer later, we'll have to do an expensive deep copy of the pixel data, using memcpy or Mat::clone().
-	
-	// Use extendedWidth instead of width to account for possible row extensions (sometimes used for memory alignment).
-	// We only need to work on columms from [0, width - 1] regardless.
-	
-	cv::Mat bgraImage = cv::Mat( (int)height, (int)extendedWidth, CV_8UC4, base );
-	
-	for ( uint32_t y = 0; y < height; y++ )
-	{
-		for ( uint32_t x = 0; x < width; x++ )
-		{
-			bgraImage.at<cv::Vec<uint8_t,4> >(y,x)[1] = 0;
-		}
-	}
-	
-	CVPixelBufferUnlockBaseAddress( pixelBuffer, 0 );
-	
-	return (CVPixelBufferRef)CFRetain( pixelBuffer );
+    CVPixelBufferLockBaseAddress( pixelBuffer, 0 );
+    
+    unsigned char *base = (unsigned char *)CVPixelBufferGetBaseAddress( pixelBuffer );
+    size_t width = CVPixelBufferGetWidth( pixelBuffer );
+    size_t height = CVPixelBufferGetHeight( pixelBuffer );
+    size_t stride = CVPixelBufferGetBytesPerRow( pixelBuffer );
+    size_t extendedWidth = stride / sizeof( uint32_t ); // each pixel is 4 bytes/32 bits
+    
+    // Since the OpenCV Mat is wrapping the CVPixelBuffer's pixel data, we must do all of our modifications while its base address is locked.
+    // If we want to operate on the buffer later, we'll have to do an expensive deep copy of the pixel data, using memcpy or Mat::clone().
+    
+    // Use extendedWidth instead of width to account for possible row extensions (sometimes used for memory alignment).
+    // We only need to work on columms from [0, width - 1] regardless.
+    
+    cv::Mat bgraImage = cv::Mat( (int)height, (int)extendedWidth, CV_8UC4, base );
+    
+    
+#if DETECT_RECT
+    debugSquares([self findSquaresInImage:bgraImage], bgraImage);
+#else
+    for ( uint32_t y = 0; y < height; y++ )
+    {
+        for ( uint32_t x = 0; x < width; x++ )
+        {
+            
+            bgraImage.at<cv::Vec<uint8_t,4> >(y,x)[1] = 0;
+            
+        }
+    }
+#endif //DETECT_RECT
+    
+    CVPixelBufferUnlockBaseAddress( pixelBuffer, 0 );
+    return (CVPixelBufferRef)CFRetain( pixelBuffer );
 }
+
+
+
+double angle( cv::Point pt1, cv::Point pt2, cv::Point pt0 ) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+
+- (std::vector<std::vector<cv::Point> >)findSquaresInImage:(cv::Mat)_image
+{
+    std::vector<std::vector<cv::Point> > squares;
+    cv::Mat pyr, timg, grayFrame, gray0(_image.size(), CV_8U), gray;
+    int thresh = 31, N = 3;
+    cv::pyrDown(_image, pyr, cv::Size(_image.cols/2, _image.rows/2));
+    cv::pyrUp(pyr, timg, _image.size());
+    //Convert smoothed image to grayscale
+    cv::cvtColor(timg, grayFrame, cv::COLOR_RGB2GRAY);
+    
+    cv::blur( grayFrame, grayFrame, cv::Size(3,3) );
+    
+    std::vector<std::vector<cv::Point> > contours;
+    
+    //if using RGB, process over all 3 channels. i.e. c<3
+    for( int c = 0; c < 1; c++ ) {
+        int ch[] = {c, 0};
+        
+        //if processing over RGB, use timg instead of grayFrame.
+        mixChannels(&grayFrame, 1, &gray0, 1, ch, 1);
+        for( int l = 0; l < N; l++ ) {
+            if( l == 0 ) {
+                cv::Canny(gray0, gray, 0, thresh, 3);
+                cv::dilate(gray, gray, cv::Mat(), cv::Point(-1,-1));
+            }
+            else {
+                gray = gray0 >= (l+1)*255/N;
+            }
+            //            cv::findContours(gray, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+            cv::findContours(gray, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+            
+            std::vector<cv::Point> approx;
+            for( size_t i = 0; i < contours.size(); i++ )
+            {
+                cv::approxPolyDP(cv::Mat(contours[i]), approx, arcLength(cv::Mat(contours[i]), true)*0.02, true);
+                if( approx.size() == 4 && fabs(contourArea(cv::Mat(approx))) > 1000 && cv::isContourConvex(cv::Mat(approx))) {
+                    double maxCosine = 0;
+                    
+                    for( int j = 2; j < 5; j++ )
+                    {
+                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+                    
+                    if( maxCosine < 0.3 ) {
+                        squares.push_back(approx);
+                    }
+                }
+            }
+        }
+    }
+    return squares;
+}
+
+void debugSquares( std::vector<std::vector<cv::Point> > squares, cv::Mat image )
+{
+    
+#if SHOW_GRAYFRAME
+    cv::Mat grayFrame;
+    cv::cvtColor(image, grayFrame, cv::COLOR_RGB2GRAY);
+    for ( int i = 0; i< squares.size(); i++ ) {
+        
+        // draw bounding rect
+        cv::Rect rect = boundingRect(cv::Mat(squares[i]));
+        // cv::rectangle(grayFrame, rect.tl(), rect.br(), cv::Scalar(0,255,0), 2, 8, 0);
+        
+    }
+    
+    
+    
+    return grayFrame;
+#else
+    cv::Rect outsideRect;
+    if(squares.size()>0){
+        outsideRect = boundingRect(cv::Mat(squares[0]));
+    }
+    
+    for ( int i = 1; i< squares.size(); i++ ) {
+        
+        //  NSLog([NSString stringWithFormat:@"%.0f %.0f, %.0f %.0f", squares])
+        // draw contour
+        // cv::drawContours(image, squares, i, cv::Scalar(255,0,0), 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
+        
+        // draw bounding rect
+        cv::Rect rect = boundingRect(cv::Mat(squares[i]));
+        
+        //If multiple rectangles are found, calculate the one that bounds the other
+        outsideRect = outsideRect | rect ;
+        //        cv::rectangle(image, rect.tl(), rect.br(), cv::Scalar(0,255,0), 2, 8, 0);
+        
+        
+        // draw rotated rect
+        /*        cv::RotatedRect minRect = minAreaRect(cv::Mat(squares[i]));
+         cv::Point2f rect_points[4];
+         minRect.points( rect_points );
+         for ( int j = 0; j < 4; j++ ) {
+         cv::line( image, rect_points[j], rect_points[(j+1)%4], cv::Scalar(0,0,255), 1, 8 ); // blue
+         }*/
+        
+    }
+    
+    //Draw the rectangle on the original image
+    cv::Mat tempImage;
+    image.copyTo(tempImage);
+    cv::rectangle(tempImage, outsideRect.tl(), outsideRect.br(), cv::Scalar(0,255,0,0.3), cv::FILLED, 8, 0);
+    //Blend the rectangle with the original image to get the transparency
+    cv::addWeighted(tempImage, 0.4, image, 1, 1, image);
+#endif //SHOW_GRAYFRAME
+    
+}
+
+
+
+
+
 
 @end
